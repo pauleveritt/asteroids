462782bccd72201c28e29d34c721c583c03c3818
faassen <faassen@startifact.com>
2016-08-02 15:39:14 +0200

    A naive pyglet experiment drawing dots.

---
+++ experiment_pyglet.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+import pyglet
+import time
+
+
+class World:
+    def __init__(self, position, velocity):
+        self.position = position
+        self.velocity = velocity
+
+
+def make_world():
+    amount = 100
+    position = pd.DataFrame({
+        'x': np.random.rand(amount),
+        'y': np.random.rand(amount)
+    })
+    velocity = pd.DataFrame({
+        'x_speed': np.random.randn(amount) / 10,
+        'y_speed': np.random.randn(amount) / 10
+    })
+    return World(position, velocity)
+
+
+def update(w, dt):
+    w.position['x'] += w.velocity['x_speed'] * dt
+    w.position['y'] += w.velocity['y_speed'] * dt
+
+
+def render(w):
+    scaled = w.position * 600
+    batch = pyglet.graphics.Batch()
+    for index, row in scaled.iterrows():
+        batch.add(1, pyglet.gl.GL_POINTS, None,
+                  ('v2i', (int(row.x), int(row.y))))
+    batch.draw()
+
+
+class Window(pyglet.window.Window):
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        self.w = make_world()
+        self.fps_display = pyglet.clock.ClockDisplay()
+
+    def on_draw(self):
+        self.clear()
+        render(self.w)
+        self.fps_display.draw()
+
+    def animate(self, delta_time):
+        update(self.w, delta_time)
+
+
+def main():
+    window = Window()
+    pyglet.clock.schedule(window.animate)
+    pyglet.app.run()
+
+
+if __name__ == '__main__':
+    main()
